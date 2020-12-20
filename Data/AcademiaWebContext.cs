0e078d8758040e8bab0695b5c7e7e9587b350d7b
rohit maharjan <rmaharjan61@gmail.com>
2020-12-20 17:54:22 +0545

    final commit

---
+++ Data/AcademiaWebContext.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Academia.Web.Data
+{
+    public class AcademiaWebContext : DbContext
+    {
+        public AcademiaWebContext (DbContextOptions<AcademiaWebContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Student> Student { get; set; }
+    }
+
+    public class AcademiaWebContext1 : DbContext
+    {
+        public AcademiaWebContext1(DbContextOptions<AcademiaWebContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Student> Student { get; set; }
+    }
+    public class AcademiaWebContext2 : DbContext
+    {
+        public AcademiaWebContext2(DbContextOptions<AcademiaWebContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Student> Student { get; set; }
+    }
+    public class AcademiaWebContext3 : DbContext
+    {
+        public AcademiaWebContext3(DbContextOptions<AcademiaWebContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Student> Student { get; set; }
+    }
+    public class AcademiaWebContext4 : DbContext
+    {
+        public AcademiaWebContext4(DbContextOptions<AcademiaWebContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Student> Student { get; set; }
+    }
+}
